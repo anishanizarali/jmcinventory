3a36a29dae55dabbb42c29602d7b67e384648ce9
Blake Calhoun <ccalhou2@samford.edu>
2018-10-05 08:20:42 -0500

    added curly braces around if statement in Users.php

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -14,7 +14,7 @@ class Users extends CI_Controller {
     }
 
     public function dashboard() {
-        if(!logged_in()) redirect('auth/login');
+        if(!logged_in()) {redirect('auth/login');}
         $data['reservations'] = $this->users->getreservations(user('id'));
         echo "Welcome back " . user('id');
         echo "<br />Your role is " . user('role_id');
